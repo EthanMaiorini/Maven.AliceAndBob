f0a7f6b27a8aa99d6c10d8c2b7590055793ebdda
Ethan <ethan@zipcoders-MacBook-Pro-4.local>
2022-02-24 22:40:06 -0500

    I completed the assignment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,30 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
+       Main newObject = new Main();
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("Please enter your name: ");
+       String input = scanner.next();
+       newObject.sendMessage(input);
+       scanner.close();
+    }
+
+    public void sendMessage(String string){
+        if (equals(string)){
+            System.out.println("Hello!");
+        }
+    }
+
+    public boolean equals(String arg) {
+        String compare = arg.toUpperCase();
+        if ((compare.equals("ALICE")) || (compare.equals("BOB"))){
+            return true;
+        }else return false;
 
     }
 }
